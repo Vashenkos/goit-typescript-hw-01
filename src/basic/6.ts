80ad2e5d05bb9b3792460ed35b06080ff635cafc
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-11-03 17:38:44 +0200

    12345

--- src/basic/6.ts
+++ src/basic/6.ts
@@ -1,24 +1,25 @@
-interface User {
-    name: string;
-    age: number;
-    email: string;
-    address?: {
-      city: string;
-      country: string;
-    };
-  }
-  
-  const mang: User = {
-    name: "Mango",
-    age: 30,
-    email: "john@example.com",
-    address: {
-      city: "New York",
-      country: "USA",
-    },
-  };
-  const mang: User = {
-    name: "Mango",
-    age: 30,
-    email: "john@example.com",
-  };
+interface User {  
+  name: string;  
+  age: number;  
+  email: string;  
+  address?: {  
+    city: string;  
+    country: string;  
+  };  
+}  
+
+const ManWithAddress: User = {  
+  name: "Mango",  
+  age: 30,  
+  email: "john@example.com",  
+  address: {  
+    city: "New York",  
+    country: "USA",  
+  },  
+};  
+
+const ManWithoutAddress: User = {  
+  name: "Mango",  
+  age: 30,  
+  email: "john@example.com",  
+};
